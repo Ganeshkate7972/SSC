09c596c6eccedf08eb729db491392bec7ecadde5
Ganeshkate7972 <ganesh.kate.official@gmail.com>
2021-09-10 12:43:39 +0530

    update

--- SSC/Besic_Master/AddBord.aspx
+++ SSC/Besic_Master/AddBord.aspx
@@ -1,32 +1,52 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Besic_Master/EditMaster.Master" AutoEventWireup="true" CodeBehind="AddBord.aspx.cs" Inherits="SSC.Besic_Master.AddBord" %>
 
-<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
-</asp:Content>
+
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <div class="container" style="height: 90vh">
-        <div class="row h-100 justify-content-center align-items-center">
-            <div class="form-row  align-items-center">
-                <div class="card text-white bg-dark mb-3">
-                    <div class="card-header">Add Bord</div>
-                    <div class="row card-body">
-                        <div class="col-auto">
-                            <label for="inlineFormInput">Name</label>
+    <form id="form1" runat="server">
+        <div class="container" style="height: 76vh">
+            <div class="row h-100 justify-content-center align-items-center">
+                <div class="form-row  align-items-center">
+                    <div class="card text-white bg-dark mb-3">
+                        <div class="card-header">
+                            <asp:Label ID="Label1" runat="server" Text="Add Bord"></asp:Label>
                         </div>
-                        <div class="col-auto">
-                            <input type="text" class="form-control mb-2" id="inlineFormInput" required placeholder="Name">
-                        </div>
-                        <div class="col-auto">
-                            <button type="submit" class="btn btn-primary mb-2">Submit</button>
-                        </div>
-                        <div class="col-auto">
-                            <button type="button" class="btn btn-primary mb-2">Back</button>
-                        </div>
-                        <div class="col-auto">
-                            <button type="reset" class="btn btn-primary mb-2">Reset</button>
+                        <div class="row card-body">
+                            <div class="col-auto">
+                                <label for="Name">Name</label>
+                            </div>
+                            <div class="col-auto">
+                                <asp:TextBox class="form-control mb-2" required placeholder="Name" ID="Name" runat="server"></asp:TextBox>
+                            </div>
+                            <div class="col-auto">
+                                <asp:Label ID="Label2" runat="server" Visible="false">
+                                    <label class="btn btn-primary abcd" runat="server">
+                                        <asp:CheckBox ID="CheckBox1" CssClass="mng" runat="server" />
+                                    </label>
+                                </asp:Label>
+                                <style>
+                                    .abcd::after {
+                                        content: "Active";
+                                    }
+                                    .mng{
+                                        opacity:.5;
+                                    }
+                                </style>
+                            </div>
+                            <div class="col-auto">
+                                <asp:Button ID="Button1" class="btn btn-primary mb-2" runat="server" OnClick="Button1_Click" Text="Submit" />
+                            </div>
+                            <div class="col-auto">
+                                <a href="BordList.aspx" class="btn btn-primary mb-2">Back</a>
+                            </div>
+                            <div class="col-auto">
+                                <button type="reset" class="btn btn-primary mb-2">Reset</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+
+
+    </form>
 </asp:Content>
--- SSC/Besic_Master/AddBord.aspx.cs
+++ SSC/Besic_Master/AddBord.aspx.cs
@@ -4,14 +4,125 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using SSC.App_Start;
 
 namespace SSC.Besic_Master
 {
-    public partial class AddBord : System.Web.UI.Page
+    public partial class AddBord : DataBase
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            Page.MaintainScrollPositionOnPostBack = true;
+            if (!IsPostBack)
+            {
+
+                string Qry = Request.QueryString["ID"];
+                if (Qry != "")
+                {
+                    ViewState["Qry"] = Qry;
+                    GetData();
+                    
+                }
+
+
+            }
         }
+
+        public void GetData()
+        {
+            DataBase db = new DataBase();
+            string Qry = Request.QueryString["ID"];
+            //string str = "select * from SMS_CLIENTSTATUS_MASTER where CSM_ID='" + Qry + "'";
+            SqlDataReader dr = ExecuteSqlDataReader("BordGETDATA", new SqlParameter("@ID", Qry));
+            while (dr.Read())
+            {
+
+                Name.Text = dr["BordName"].ToString();
+                CheckBox1.Checked = dr.GetBoolean(2);
+                Label2.Visible = true;
+                Label1.Text = "Edit Bord";
+            }
+
+        }
+        public void AddUpdateData()
+        {
+            try
+            {
+                string connectionstring = ConfigurationManager.ConnectionStrings["strCone"].ConnectionString;
+                SqlConnection con = new SqlConnection(connectionstring);
+                string ID = Convert.ToString(ViewState["Qry"]);
+                //string query = "Insert Into VMPCountryMaster(Name,Status,CreatedBy) values(@name,@Status,@CreatedBy)";
+                //SqlCommand cm = new SqlCommand(query, con);
+                SqlCommand cm = new SqlCommand("BordInsertUpdate", con);
+                cm.CommandType = CommandType.StoredProcedure;
+                cm.Parameters.AddWithValue("@ID", ID);
+                cm.Parameters.AddWithValue("@Name", Name.Text);
+                if (CheckBox1.Checked)
+                {
+                    cm.Parameters.AddWithValue("@Isdelete", 1);
+
+
+                }
+                else if (!CheckBox1.Checked)
+                {
+                    cm.Parameters.AddWithValue("@Isdelete", 0);
+
+
+                }
+                con.Open();
+                cm.ExecuteNonQuery();
+                con.Close();
+                ClientScript.RegisterStartupScript(this.Page.GetType(), "validation", "<script language='javascript'>swal({title:'Data is successfully save',icon:'success'}).then(function() {window.location = 'BordList';});</script>");
+
+
+            }
+            catch (Exception ex) { }
+            finally
+            {
+                SqlConnection.ClearAllPools();
+            }
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //AddUpdateData();
+            string ID = Convert.ToString(ViewState["Qry"]);
+            List<string> locationList = new List<string>();
+            DataTable dt = ExecuteDataTable("BordNameCKList", new SqlParameter("@ID", ID));
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                locationList.Add(Convert.ToString(dt.Rows[i].ItemArray[0]).ToUpper());
+            }
+            if (ID == "" || ID == null)
+            {
+                //DataTable dt= ExecuteDataTable("SMS_ClientTypePayment_CkList", new SqlParameter("@ID", ID));
+                if (locationList.Contains(Name.Text.ToUpper()) == true)
+                {
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "Popup", "showModal1();", true);
+                }
+                else
+                {
+                    AddUpdateData();
+                }
+            }
+            else if (ID != "" || ID != null)
+            {
+                //DataTable dt = ExecuteDataTable("SMS_ClientTypePayment_CkList", new SqlParameter("@ID", ID));
+                if (locationList.Contains(Name.Text.ToUpper()) == true)
+                {
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "Popup", "showModal1();", true);
+                }
+                else
+                {
+                    AddUpdateData();
+                }
+            }
+        }
+
+
+
     }
 }
